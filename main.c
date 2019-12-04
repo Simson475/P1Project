4dbd9973c62cd221d00bd1ca609c9c799d45e996
Jacob Guldhammer <jgje18@student.aau.dk>
2019-12-04 13:36:42 +0100

    Change main.c

--- main.c
+++ main.c
@@ -7,36 +7,32 @@
 #include "Samfund.h"
 #include "Sundhed.h"
 #include "Teknisk.h"
+#include "fakultet.h"
 
+void Choose_faculty();
 
 int main(void){
-    int i, question_amount;
-    fakulteter_struct *fakultet= calloc(FAKULTETSIZE,sizeof(fakulteter_struct));
-    weight *weights = calloc(MAXEDUCATIONS,sizeof(weight));
-
-    question_amount=load_questions(weights, fakultetsvalg);
-    get_questions(fakultet, weights,question_amount);
-
-    for (i=0;i<FAKULTETSIZE;i++){
-
-        switch (i) {
-            case 0:
-                printf("Humaniora         "); break;
-            case 1:
-                printf("Naturvidenskab    "); break;
-            case 2:
-                printf("Teknik            "); break;
-            case 3:
-                printf("Samfundsvidenskab "); break;
-            case 4:
-                printf("Sundhedsvidenskab "); break;
-            default:
-                printf("Noget gik galt i printet."); break;
-        }
-
-        printf(" %.2lf \n",fakultet[i].score);
-    }
-
+    Decide_faculty();
+    Choose_faculty();
 	return 0; 
 	
+}
+
+void Choose_faculty(){ /*Funktion hvor brugeren vælger ønsket studie*/
+    int Faculty_Input;
+    printf("Vaelg dit oenskede fakultet:\n");
+    printf("0 : Humaniora\n 1 : Naturvidenskab\n2 : Teknisk\n 3 : Samfundsfagligt\n 4 : Sundheds\n");
+    scanf("%d",&Faculty_Input);
+
+    switch (Faculty_Input){
+        case Humaniora: Decide_Human();break;
+        case Natur: Decide_Nature();break;
+        case Teknisk: Decide_Teknisk();break;
+        case Samfund: Decide_Samfund();break;
+        case Sundhed: Decide_Sundhed();break;
+        default:break;
+    }
+
+    return;
+
 }
