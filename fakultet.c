f8d774da99bc9c2c554f62c8aa19be7bd14460d5
Simon Nielsen <Simonhelmingnielsen@gmail.com>
2019-12-06 10:00:21 +0100

    fixed

--- fakultet.c
+++ fakultet.c
@@ -6,7 +6,7 @@
 
 void Decide_faculty(){
     int i, question_amount;
-    fakulteter_struct *fakultet = calloc(FAKULTETSIZE,sizeof(fakulteter_struct));
+    fakulteter_struct *fakultet = calloc(MAXEDUCATIONS,sizeof(fakulteter_struct));
     weight *weights = calloc(MAXEDUCATIONS,sizeof(weight));
 
     question_amount = load_questions(weights, fakultetsvalg, fakultet);
