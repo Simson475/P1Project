f0be7a5360d24fb98e191d22501897f951b2c2ee
Jacob Guldhammer <jgje18@student.aau.dk>
2019-12-06 12:59:55 +0100

    added user guidence

--- Teknisk.c
+++ Teknisk.c
@@ -9,6 +9,7 @@ void Decide_Teknisk(char* Name){
     fakulteter_struct *teknisk_fakultet = calloc(MAXEDUCATIONS, sizeof(fakulteter_struct));
     weight *weights = calloc(MAXEDUCATIONS, sizeof(weight));
 
+    printf("Besvar foelgende: \n");
     question_amount = load_questions(weights, Teknisk, teknisk_fakultet);
     get_questions(teknisk_fakultet, weights, question_amount);
     sort_by_score(teknisk_fakultet);
--- utility.c
+++ utility.c
@@ -145,6 +145,7 @@ char* Get_users_name(){
     char* Name = calloc(NAME_SIZE,sizeof(char));
     printf("Indtast navn: \n");
     scanf("%s", Name);
+    printf("---------------------------------------------------\n");
 
     return Name;
 }
